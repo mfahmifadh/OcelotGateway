3c1358bfaecbf7b5254239996ba3f9e29fe65823
mfahmifadh <mfahmifadh@gmail.com>
2021-07-08 21:36:23 +0700

    ocelot json

--- OcelotGateway/Program.cs
+++ OcelotGateway/Program.cs
@@ -23,7 +23,7 @@ namespace OcelotGateway
                     var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
                     webBuilder.UseStartup<Startup>();
                     webBuilder.ConfigureAppConfiguration(config =>
-                    config.AddJsonFile($"ocelot.{env}.json"));
+                    config.AddJsonFile($"ocelot.json"));
                 })
             .ConfigureLogging(logging => logging.AddConsole());
     }
